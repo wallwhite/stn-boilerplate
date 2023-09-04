c074834032120d32d364e92391d5078b6f689a17
Yaroslav Romanenko <yarikrom05@gmail.com>
2023-09-04 18:27:17 +0300

    feat: add api constants

---
+++ src/lib/api/api.constants.ts
@@ -0,0 +1,29 @@
+export const CHATS_LIST = [
+  {
+    id: 1,
+    title: 'Some Chat',
+    lastMessage: "I'd like to know more about next.js",
+    icon: '🦧',
+    color: '#3F51B5',
+    counter: 23,
+    createdAt: Date.now().toString(),
+  },
+  {
+    id: 2,
+    title: 'Another Chat',
+    lastMessage: 'I like your blog!',
+    icon: '🐵',
+    color: '#FF4081',
+    counter: 1,
+    createdAt: Date.now().toString(),
+  },
+  {
+    id: 3,
+    title: 'Third Chat',
+    lastMessage: 'What the f*ck is this, and what am I doing here?',
+    icon: '🙈',
+    color: '#FFD740',
+    counter: 999,
+    createdAt: Date.now().toString(),
+  },
+];
