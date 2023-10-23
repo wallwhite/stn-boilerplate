345b9539e97ed701608df4fce29fb098245aa4b5
Yaroslav Romanenko <yarikrom05@gmail.com>
2023-10-23 21:11:48 +0300

    feat: complete prisma schema

---
+++ src/lib/api/prisma.api.ts
@@ -0,0 +1,25 @@
+// Queries
+export const getMockUser = async (): Promise<void> => {};
+
+export const getMockUserId = async (): Promise<void> => {};
+
+export const getChatsList = async (): Promise<void> => {};
+
+export const getCategoriesList = async (): Promise<void> => {};
+
+export const getCategoryById = async (): Promise<void> => {};
+
+export const getChatById = async (): Promise<void> => {};
+
+export const getMessagesByChatId = async (): Promise<void> => {};
+
+// Mutations
+export const createCategory = async (): Promise<void> => {};
+
+export const createMessage = async (): Promise<void> => {};
+
+export const createChat = async (): Promise<void> => {};
+
+export const deleteChats = async (): Promise<void> => {};
+
+export const deleteChatById = async (): Promise<void> => {};
