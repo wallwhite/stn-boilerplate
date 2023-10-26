3f7f2186779f0e27d51aecf6695932f579f69833
Yaroslav Romanenko <yarikrom05@gmail.com>
2023-10-26 17:08:53 +0300

    feat: add create message endpoint

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,3 +1,20 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { createMessage, getChatById, getMockUserId } from '@/lib/api/prisma.api';
 
-export const POST = async (): Promise<NextResponse> => NextResponse.next();
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  const body = await req.json();
+
+  const userId = await getMockUserId();
+  const chat = await getChatById(body?.chatId);
+
+  const message = await createMessage(
+    {
+      content: body?.content,
+      role: 'user',
+    },
+    chat?.id,
+    userId,
+  );
+
+  return NextResponse.json(message);
+};
