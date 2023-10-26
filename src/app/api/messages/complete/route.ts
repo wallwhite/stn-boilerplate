783c190a259c4ada540872b1937489be0dd54d45
Yaroslav Romanenko <yarikrom05@gmail.com>
2023-10-26 17:39:11 +0300

    feat: add message completion endpoint

--- src/app/api/messages/complete/route.ts
+++ src/app/api/messages/complete/route.ts
@@ -1,16 +1,41 @@
-import { NextResponse } from 'next/server';
-// import { Message } from '@prisma/client';
-// import OpenAI from 'openai';
-// import { ChatCompletionMessageParam } from 'openai/resources/chat/index.mjs';
+import { NextRequest, NextResponse } from 'next/server';
+import { Message } from '@prisma/client';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources/chat/index.mjs';
+import { CHATS_CREATIVITY } from '@/lib/api/api.constants';
+import { createMessage, getChatById, getMessagesByChatId } from '@/lib/api/prisma.api';
 
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY || '',
-// });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || '',
+});
 
-// const formatMessagesForAi = (messages: Message[]): ChatCompletionMessageParam[] =>
-//   messages.map((message) => ({
-//     role: message.role,
-//     content: message.content,
-//   }));
+const formatMessagesForAi = (messages: Message[]): ChatCompletionMessageParam[] =>
+  messages.map((message) => ({
+    role: message.role,
+    content: message.content,
+  }));
 
-export const POST = async (): Promise<NextResponse> => NextResponse.json({});
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  const body = await req.json();
+
+  const chat = await getChatById(body?.chatId);
+
+  const systemMessage = await getMessagesByChatId(chat?.id, 1, 'asc');
+  const messages = (await getMessagesByChatId(chat?.id, 10)).reverse();
+
+  const chatPreset = CHATS_CREATIVITY[chat?.creativity ?? 'medium'];
+
+  const completion = await openai.chat.completions.create({
+    ...chatPreset,
+    messages: formatMessagesForAi([...systemMessage, ...messages] as Message[]),
+  });
+
+  const { role, content } = completion.choices[0].message;
+
+  const message = await createMessage(
+    { role, content } as Pick<Message, 'content' | 'role'>,
+    chat?.id,
+  );
+
+  return NextResponse.json(message);
+};
